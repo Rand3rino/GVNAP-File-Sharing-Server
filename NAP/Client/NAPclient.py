7d66e57c8a30b6693b33de969cadc31db073ef9e
alecbetancourt <36958016+alecbetancourt@users.noreply.github.com>
2019-03-25 20:38:10 -0400

    Update NAPclient.py

--- NAP/Client/NAPclient.py
+++ NAP/Client/NAPclient.py
@@ -1,39 +1,33 @@
-# Python FTP Client
+# Python NAP Client
 import socket 
 
 while True:
 	command = input("Input a command: ")
 	split = command.split()
-	#debug
-	for s in split:
-		print(s)
 	if split[0] == "CONNECT":
 		sock = socket.socket()
 		sock.connect((split[1], int(split[2])))
 		print("Connected")
+		sock.send(command.encode())
 	if split[0] == "STORE":
 		sock.send(command.encode())
 		with open(split[1], 'rb') as f:
 			sock.sendfile(f, 0)
-		#f = open(split[1],'rb')
-		#l = f.read(1024)
-		#while (l):
-		#	print("Sending...")
-		#	sock.sendall(l)
-		#	l = f.read(1024)
-		#f.close()
 		print("Done Sending")
-		#sock.shutdown(socket.SHUT_WR)
 	if split[0] == "RETRIEVE":
 		sock.send(command.encode())
-		f = open(split[1],'wb')
-		l = sock.recv(1024)
-		while (l):
-			print("Receiving...")
-			f.write(l)
-			l = sock.recv(1024)
-		f.close()
-		print("Done Receiving")
+		with open(split[1],'wb') as f:
+				f = open(split[1],'wb')
+				#print "Receiving..."
+				l = sock.recv(1024)
+				while (len(l)==1024):
+					print(len(l))
+					print("Storing...")
+					f.write(l)
+					l = sock.recv(1024)
+				f.write(l)
+				f.close()
+				print("Done Receiving")
 	if split[0] == "QUIT":
 		sock.send(command.encode())
 		sock.close()
