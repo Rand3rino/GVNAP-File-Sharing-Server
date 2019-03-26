24e7aa5f5794c0d5036cb641837d1359ad6e074f
alecbetancourt <36958016+alecbetancourt@users.noreply.github.com>
2019-03-25 20:37:50 -0400

    Update NAPserver.py

--- NAP/NAPserver.py
+++ NAP/NAPserver.py
@@ -2,8 +2,7 @@
 #multi thread
 #add nap functionality
 
-
-#Python FTP Server
+#Python NAP Server
 import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
@@ -20,31 +19,23 @@ while True:
 	while True:
 		command = conn.recv(1024).decode()
 		split = command.split()
-		#for piece in split:
-		#	print(piece)
+
 		if split[0] == "STORE":
 			with open(split[1],'wb') as f:
+				print("Storing...")
 				f = open(split[1],'wb')
-				#print "Receiving..."
 				l = conn.recv(1024)
 				while (len(l)==1024):
 					print(len(l))
-					print("Storing...")
 					f.write(l)
 					l = conn.recv(1024)
 				f.write(l)
 				f.close()
 				print("Done Storing")
-				#conn.shutdown(socket.SHUT_WR)
 		elif split[0] == "RETRIEVE":
-			f = open(split[1],'rb')
-			#print 'Sending...'
-			l = f.read(1024)
-			while (l):
-				print("Sending...")
-				conn.send(l)
-				l = f.read(1024)
-			f.close()
+			print("Sending...")
+			with open(split[1], 'rb') as f:
+				conn.sendfile(f, 0)
 			print("Done Sending")
 		elif split[0] == "QUIT":
 			conn.close()
