aaedcc323cd98c3bfeefb60648ed7bc0a86a0024
alecbetancourt <36958016+alecbetancourt@users.noreply.github.com>
2019-03-26 22:10:39 -0400

    Update NAPclient.py

--- NAP/Client/NAPclient.py
+++ NAP/Client/NAPclient.py
@@ -4,31 +4,28 @@ import socket
 while True:
 	command = input("Input a command: ")
 	split = command.split()
+	#CONNECT ip port uname cspeed
 	if split[0] == "CONNECT":
 		sock = socket.socket()
 		sock.connect((split[1], int(split[2])))
 		print("Connected")
 		sock.send(command.encode())
-	if split[0] == "STORE":
+	#UPLOAD fname desc
+	if split[0] == "UPLOAD":
 		sock.send(command.encode())
-		with open(split[1], 'rb') as f:
-			sock.sendfile(f, 0)
-		print("Done Sending")
-	if split[0] == "RETRIEVE":
+	#SEARCH fname
+	if split[0] == "SEARCH":
 		sock.send(command.encode())
-		with open(split[1],'wb') as f:
-				f = open(split[1],'wb')
-				#print "Receiving..."
-				l = sock.recv(1024)
-				while (len(l)==1024):
-					print(len(l))
-					print("Storing...")
-					f.write(l)
-					l = sock.recv(1024)
-				f.write(l)
-				f.close()
-				print("Done Receiving")
+		info = sock.recv(1024).decode()
+		print(info)
+	#TABLES
+	if split[0] == "TABLES":
+		sock.send(command.encode())
+	#QUIT
 	if split[0] == "QUIT":
 		sock.send(command.encode())
 		sock.close()
 		break
+	if split[0] == "STOP":
+		sock.send(command.encode())
+	#use elif to error handle
