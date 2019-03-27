8db4921d5aa425f941b00504c112e80d2f67c2d3
alecbetancourt <36958016+alecbetancourt@users.noreply.github.com>
2019-03-26 22:10:17 -0400

    Update NAPserver.py

--- NAP/NAPserver.py
+++ NAP/NAPserver.py
@@ -5,6 +5,11 @@
 #Python NAP Server
 import socket               # Import socket module
 
+#stores hostname key to username, port, connection speed
+users = {}
+#stores filename key to hostname, file desc
+files = {}
+
 s = socket.socket()         # Create a socket object
 #host = socket.gethostname() # Get local machine name
 #print(host)
@@ -15,31 +20,44 @@ s.listen(4)
 while True:
 	print("Waiting for connection...")
 	conn, addr = s.accept()
-	print("Got connection from:", addr)
+	print("Got connection from:", addr)	
 	while True:
 		command = conn.recv(1024).decode()
 		split = command.split()
-
-		if split[0] == "STORE":
-			with open(split[1],'wb') as f:
-				print("Storing...")
-				f = open(split[1],'wb')
-				l = conn.recv(1024)
-				while (len(l)==1024):
-					print(len(l))
-					f.write(l)
-					l = conn.recv(1024)
-				f.write(l)
-				f.close()
-				print("Done Storing")
-		elif split[0] == "RETRIEVE":
-			print("Sending...")
-			with open(split[1], 'rb') as f:
-				conn.sendfile(f, 0)
-			print("Done Sending")
+		#for piece in split:
+		#	print(piece)
+		if split[0] == "CONNECT":
+			users[addr[0]] = [split[3], str(addr[1]), split[4]]
+			print("User indexed in users table")
+		#make multi word desc work
+		elif split[0] == "UPLOAD":
+			files[split[1]] = [addr[0], split[2]]
+			print("File indexed in files table")
+		elif split[0] == "SEARCH":
+			if split[1] in files:
+				#result = 
+				conn.send((' '.join(files[split[1]]) + ' ' + ' '.join(users[files[split[1]][0]])).encode())
+		elif split[0] == "TABLES":
+			print("Users")
+			for k in users:
+				print("Key: ",k)
+				print("Value: ", users[k])
+			print("Files")
+			for k in files:
+				print("Key: ",k)
+				print("Value: ", files[k])
 		elif split[0] == "QUIT":
+			#delete info from tables
+			for file in files:
+				if users[addr[0]][0] == file[0]:
+					del file
+			print("Files deleted from files table")
+			del users[addr[0]]
+			print("User deleted from users table")
 			conn.close()
 			break
+		elif split[0] == "STOP":
+			exit()
 		#invalid command
 		else:
 			print("Bad command")
